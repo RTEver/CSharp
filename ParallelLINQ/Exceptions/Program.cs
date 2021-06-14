b6a92e0eb2fca30ad78251f6714f1e20c2af2a4a
RTEver <RimuruTempest.Everything@gmail.com>
2021-06-14 19:44:25 +0300

    ~

---
+++ ParallelLINQ/Exceptions/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal static class Program : Object
+    {
+        private static void Main(String[] args)
+        {
+            Console.WriteLine(                      "+++++++++++++++++++++++++++++" + Environment.NewLine);
+
+            Exercise_1();
+
+            Console.WriteLine(Environment.NewLine + "+++++++++++++++++++++++++++++" + Environment.NewLine);
+
+            Exercise_2();
+
+            Console.WriteLine(Environment.NewLine + "+++++++++++++++++++++++++++++" + Environment.NewLine);
+
+            Exercise_3();
+
+            Console.WriteLine(Environment.NewLine + "+++++++++++++++++++++++++++++" + Environment.NewLine);
+
+            Exercise_4();
+
+            Console.WriteLine(Environment.NewLine + "+++++++++++++++++++++++++++++");
+        }
+
+        private static Int32 Factorial(Int32 n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return (n > 1) ? n * Factorial(n - 1) : 1;
+        }
+
+        private static void Exercise_1()
+        {
+            var numbers = new Object[] { 1, 2, 3, 4, 5, "hello", "World!" };
+
+            var factorials = from number in numbers.AsParallel<Object>()
+                             let x = (Int32)number
+                             select new
+                             {
+                                 StartNumber = number,
+                                 Result = Factorial(x),
+                             };
+            try
+            {
+                factorials.ForAll(factorial => Console.WriteLine(factorial.StartNumber + "! = " + factorial.Result));
+            }
+            catch (AggregateException ex)
+            {
+                foreach (Exception e in ex.InnerExceptions)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private static void Exercise_2()
+        {
+            var cts = new CancellationTokenSource();
+
+            new Task(() =>
+            {
+                Thread.Sleep(4);
+                cts.Cancel();
+            }).Start();
+
+            try
+            {
+                var numbers = new Int32[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+                var factorials = from number in numbers.AsParallel().AsOrdered<Int32>().WithCancellation(cts.Token)
+                                 select Factorial(number);
+
+                foreach (Int32 number in factorials)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Операция была прервана");
+            }
+            catch (AggregateException ex)
+            {
+                if (ex.InnerExceptions != null)
+                {
+                    foreach (Exception e in ex.InnerExceptions)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Non inner exception...");
+                }
+            }
+            finally
+            {
+                cts.Dispose();
+            }
+        }
+
+        private static void Exercise_3()
+        {
+            var cts = new CancellationTokenSource();
+
+            new Task(() =>
+            {
+                Thread.Sleep(4);
+                cts.Cancel();
+            }).Start();
+
+            try
+            {
+                var numbers = new Int32[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+                var factorials = from number in numbers.AsParallel().WithCancellation(cts.Token)
+                                 select Factorial(number);
+
+                foreach (Int32 number in factorials)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Операция была прервана");
+            }
+            catch (AggregateException ex)
+            {
+                if (ex.InnerExceptions != null)
+                {
+                    foreach (Exception e in ex.InnerExceptions)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Non inner exception...");
+                }
+            }
+            finally
+            {
+                cts.Dispose();
+            }
+        }
+
+        private static void Exercise_4()
+        {
+            var cts = new CancellationTokenSource();
+
+            new Task(() =>
+            {
+                Thread.Sleep(10000);
+                cts.Cancel();
+            }).Start();
+
+            try
+            {
+                var numbers = new Object[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, "Hello", "World!" };
+
+                var factorials = from number in numbers.AsParallel<Object>().AsOrdered<Object>().WithCancellation(cts.Token)
+                                 let x = (Int32)number
+                                 select Factorial(x);
+
+                foreach (Int32 number in factorials)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Операция была прервана");
+            }
+            catch (AggregateException ex)
+            {
+                if (ex.InnerExceptions != null)
+                {
+                    foreach (Exception e in ex.InnerExceptions)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Non inner exception...");
+                }
+            }
+            finally
+            {
+                cts.Dispose();
+            }
+        }
+    }
+}
---
+++ ParallelLINQ/Introduction/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Linq;
+
+namespace Introduction
+{
+    internal static class Program : Object
+    {
+        private static void Main(String[] args)
+        {
+            Console.WriteLine(Environment.NewLine + "------------------------------" + Environment.NewLine);
+            
+            Console.WriteLine("ForAll" + Environment.NewLine);
+
+            Example_1();
+
+            Console.WriteLine(Environment.NewLine + "------------------------------" + Environment.NewLine);
+
+            Console.WriteLine("AsOrdered with ForAll" + Environment.NewLine);
+
+            Example_2();
+
+            Console.WriteLine(Environment.NewLine + "------------------------------" + Environment.NewLine);
+
+            Console.WriteLine("AsOrdered with foreach" + Environment.NewLine);
+
+            Example_2_2();
+
+            Console.WriteLine(Environment.NewLine + "------------------------------" + Environment.NewLine);
+
+            Console.WriteLine("AsOrdered + AsUnordered with foreach" + Environment.NewLine);
+
+            Example_2_3();
+
+            Console.WriteLine(Environment.NewLine + "------------------------------" + Environment.NewLine);
+
+            Console.WriteLine("AsUnordered" + Environment.NewLine);
+
+            Example_3();
+
+            Console.WriteLine(Environment.NewLine + "------------------------------" + Environment.NewLine);
+
+            Console.WriteLine("AsOrdered result" + Environment.NewLine);
+
+            Example_3_2();
+
+            Console.WriteLine(Environment.NewLine + "------------------------------" + Environment.NewLine);
+
+            Console.WriteLine("AsOrdered with AsUnordered" + Environment.NewLine);
+
+            Example_3_3();
+
+            Console.WriteLine(Environment.NewLine + "------------------------------" + Environment.NewLine);
+        }
+
+        private static Int32 Factorial(Int32 n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return (n > 1) ? n * Factorial(n - 1) : 1;
+        }
+
+        private static void Example_1()
+        {
+            var numbers = new[] { -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            (
+             from number in numbers.AsParallel<Int32>()
+             where number >= 0
+             select ("Factorial " + number + " = " + Factorial(number))
+            ).ForAll<String>(Console.WriteLine);
+        }
+
+        private static void Example_2()
+        {
+            var numbers = new[] { -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            (
+             from number in numbers.AsParallel<Int32>().AsOrdered<Int32>()
+             where number >= 0
+             select ("Factorial " + number + " = " + Factorial(number))
+            ).ForAll<String>(Console.WriteLine);
+        }
+
+        private static void Example_2_2()
+        {
+            var numbers = new[] { -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var factorials = from number in numbers.AsParallel<Int32>().AsOrdered<Int32>()
+                             where number >= 0
+                             select ("Factorial " + number + " = " + Factorial(number));
+
+            foreach (String result in factorials)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        private static void Example_2_3()
+        {
+            var numbers = new[] { -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var factorials = from number in numbers.AsParallel<Int32>().AsOrdered<Int32>().AsUnordered<Int32>()
+                             where number >= 0
+                             select ("Factorial " + number + " = " + Factorial(number));
+
+            foreach (String result in factorials)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        private static void Example_3()
+        {
+            var numbers = new[] { -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var factorials = from number in numbers.AsParallel<Int32>().AsOrdered<Int32>()
+                             where number >= 0
+                             select Factorial(number);
+
+            (
+             from factorial in factorials.AsUnordered<Int32>()
+             where factorial > 100
+             select factorial
+            ).ForAll<Int32>(Console.WriteLine);
+        }
+
+        private static void Example_3_2()
+        {
+            var numbers = new[] { -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var factorials = from number in numbers.AsParallel<Int32>().AsOrdered<Int32>()
+                             where number >= 0
+                             select Factorial(number);
+
+            (
+             from factorial in factorials
+             where factorial > 100
+             select factorial
+            ).ForAll<Int32>(Console.WriteLine);
+        }
+
+        private static void Example_3_3()
+        {
+            var numbers = new[] { -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var factorials = from number in numbers.AsParallel<Int32>().AsOrdered<Int32>().AsUnordered<Int32>()
+                             where number >= 0
+                             select Factorial(number);
+
+            (
+             from factorial in factorials
+             where factorial > 100
+             select factorial
+            ).ForAll<Int32>(Console.WriteLine);
+        }
+    }
+}
