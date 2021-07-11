0303fa921b58bdacd1708ae4be077f885fafba32
RTEver <RimuruTempest.Everything@gmail.com>
2021-07-11 07:57:12 +0300

    ~

---
+++ Rihter_Examples/Encoding_Examples/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+
+namespace Encoding_Examples
+{
+    internal static class Program : Object
+    {
+        private static void Main(String[] args)
+        {
+            Example_1_UTF_8();
+
+            Console.WriteLine(Environment.NewLine);
+
+            Example_2_Encoding_Class_Properties();
+
+            Console.WriteLine(Environment.NewLine);
+
+            Example_3_Base64();
+        }
+
+        private static void Example_1_UTF_8()
+        {
+            var exampleString = "Hello, World!";
+            
+            var encodingUTF8 = Encoding.UTF8;
+            
+            var encodedBytes = encodingUTF8.GetBytes(exampleString);
+
+            Console.WriteLine("Encoded bytes: " +
+                BitConverter.ToString(encodedBytes));
+
+            var decodedString = encodingUTF8.GetString(encodedBytes);
+
+            Console.WriteLine("Decoded string: " + decodedString);
+        }
+
+        private static void Example_2_Encoding_Class_Properties()
+        {
+            foreach (EncodingInfo ei in Encoding.GetEncodings())
+            {
+                Encoding e = ei.GetEncoding();
+
+                Console.WriteLine("{1}{0}" +
+                    "\tCodePage={2}, WindowsCodePage={3}{0}" +
+                    "\tWebName={4}, HeaderName={5}, BodyName={6}{0}" +
+                    "\tIsBrowserDisplay={7}, IsBrowserSave={8}{0}" +
+                    "\tIsMailNewsDisplay={9}, IsMailNewsSave={10}{0}",
+
+                    Environment.NewLine,
+                    e.EncodingName, e.CodePage, e.WindowsCodePage,
+                    e.WebName, e.HeaderName, e.BodyName,
+                    e.IsBrowserDisplay, e.IsBrowserSave,
+                    e.IsMailNewsDisplay, e.IsMailNewsSave);
+            }
+        }
+
+        private static void Example_3_Base64()
+        {
+            var bytes = new Byte[10];
+            new Random().NextBytes(bytes);
+
+            Console.WriteLine(BitConverter.ToString(bytes));
+
+            var s = Convert.ToBase64String(bytes);
+            Console.WriteLine(s);
+
+            bytes = Convert.FromBase64String(s);
+            Console.WriteLine(BitConverter.ToString(bytes));
+        }
+    }
+}
---
+++ Rihter_Examples/StringFormats/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace StringFormats
+{
+    internal static class Program : Object
+    {
+        private static void Main(String[] args)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendFormat(new BoldInt32s(), "{0} {1} {2:M}", "Jeff", 123, DateTime.Now);
+
+            Console.WriteLine(sb);
+        }
+    }
+
+    internal sealed class BoldInt32s : IFormatProvider, ICustomFormatter
+    {
+        public Object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+            {
+                return this;
+            }
+
+            return Thread.CurrentThread.CurrentCulture.GetFormat(formatType);
+        }
+
+        public String Format(String format, Object arg, IFormatProvider formatProvider)
+        {
+            String s;
+
+            IFormattable formattable = arg as IFormattable;
+
+            if (formattable == null)
+            {
+                s = arg.ToString();
+            }
+            else
+            {
+                s = formattable.ToString(format, formatProvider);
+            }
+
+            if (arg.GetType() == typeof(Int32))
+            {
+                return "<B>" + s + "</B>";
+            }
+
+            return s;
+        }
+    }
+}
