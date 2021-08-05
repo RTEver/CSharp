622c59d5190bf2369ce5acf520b32959f7fcfc4b
RTEver <RimuruTempest.Everything@gmail.com>
2021-08-05 21:03:59 +0300

    ~

---
+++ WindowsServices/FileWatcherService/Logger.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace FileWatcherService
+{
+    internal sealed class Logger : Object
+    {
+        private FileSystemWatcher watcher;
+        private Object locker;
+        private Boolean enabled;
+
+        public Logger()
+            : base()
+        {
+            locker = new Object();
+            enabled = true;
+
+            watcher = new FileSystemWatcher("D:\\Temp");
+            watcher.Created += Watcher_Created;
+            watcher.Changed += Watcher_Changed;
+            watcher.Deleted += Watcher_Deleted;
+            watcher.Renamed += Watcher_Renamed;
+        }
+
+        public void Start()
+        {
+            watcher.EnableRaisingEvents = true;
+
+            while (enabled)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void Stop()
+        {
+            watcher.EnableRaisingEvents = false;
+
+            enabled = false;
+        }
+
+        private void RecordEntry(String fileEvent, String filePath)
+        {
+            lock (locker)
+            {
+                using (var writer = new StreamWriter("D:\\templog.txt", true))
+                {
+                    writer.WriteLine(String.Format("{0} File {1} was {2}",
+                        DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"), filePath, fileEvent));
+
+                    writer.Flush();
+                }
+            }
+        }
+
+        private void Watcher_Renamed(Object sender, RenamedEventArgs e)
+        {
+            var fileEvent = "renamed in " + e.FullPath;
+
+            var filePath = e.OldFullPath;
+
+            RecordEntry(fileEvent, filePath);
+        }
+
+        private void Watcher_Deleted(Object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "deleted";
+
+            string filePath = e.FullPath;
+
+            RecordEntry(fileEvent, filePath);
+        }
+
+        private void Watcher_Changed(Object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "changed";
+
+            string filePath = e.FullPath;
+
+            RecordEntry(fileEvent, filePath);
+        }
+
+        private void Watcher_Created(Object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "created";
+
+            string filePath = e.FullPath;
+
+            RecordEntry(fileEvent, filePath);
+        }
+    }
+}
---
+++ WindowsServices/FileWatcherService/Service.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.ServiceProcess;
+
+namespace FileWatcherService
+{
+    public partial class Service : ServiceBase
+    {
+        private Logger logger;
+
+        public Service()
+        {
+            InitializeComponent();
+
+            CanStop = true;
+            CanPauseAndContinue = true;
+            AutoLog = true;
+        }
+
+        protected override void OnStart(String[] args)
+        {
+            logger = new Logger();
+
+            var loggerThread = new Thread(new ThreadStart(logger.Start));
+
+            loggerThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            logger.Stop();
+
+            Thread.Sleep(1000);
+        }
+    }
+}
