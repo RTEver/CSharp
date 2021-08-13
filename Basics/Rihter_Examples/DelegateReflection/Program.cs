d99122c509fa5d9329a79ddd8c8505dbba2cc700
RTEver <RimuruTempest.Everything@gmail.com>
2021-08-13 08:48:22 +0300

    ~

--- Basics/Rihter_Examples/DelegateReflection/Program.cs
+++ Basics/Rihter_Examples/DelegateReflection/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -8,9 +7,9 @@ namespace DelegateReflection
     internal delegate Object TwoInt32s(Int32 n1, Int32 n2);
     internal delegate Object OneString(String s1);
 
-    internal static class Program : Object
+    public static class Program : Object
     {
-        private static void Main(String[] args)
+        public static void Main(String[] args)
         {
             if (args.Length < 2)
             {
@@ -32,7 +31,65 @@ namespace DelegateReflection
                 return;
             }
 
-            var delType = Type.GetType(args[0]);
+            var delType = Type.GetType("DelegateReflection." + args[0], false, false);
+
+            if (delType == null)
+            {
+                Console.WriteLine("Invalid delType argument: " + args[0]);
+
+                return;
+            }
+
+            var d = default(Delegate);
+
+            try
+            {
+                var mi =
+                    typeof(Program).GetTypeInfo().GetDeclaredMethod(args[1]);
+
+                d = mi.CreateDelegate(delType);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid methodName argument: " + args[1]);
+
+                return;
+            }
+
+            var callbackArgs = new Object[args.Length - 2];
+
+            if (d.GetType() == typeof(TwoInt32s))
+            {
+                try
+                {
+                    for (var a = 2; a < args.Length; ++a)
+                    {
+                        callbackArgs[a - 2] = Int32.Parse(args[a]);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Parameters must be integers.");
+
+                    return;
+                }
+            }
+
+            if (d.GetType() == typeof(OneString))
+            {
+                Array.Copy(args, 2, callbackArgs, 0, callbackArgs.Length);
+            }
+
+            try
+            {
+                var result = d.DynamicInvoke(callbackArgs);
+
+                Console.WriteLine("Result = " + result);
+            }
+            catch (TargetParameterCountException)
+            {
+                Console.WriteLine("Incorrect number of parameters specified.");
+            }
         }
 
         private static Object Add(Int32 n1, Int32 n2)
