43e6855d409fdca6e62e1cd7334d416d92d6420f
RTEver <RimuruTempest.Everything@gmail.com>
2021-11-15 00:01:37 +0300

    ~

---
+++ ADO.NET (.NET 5)/Mobile shop project/MobileShop/DatabaseBuilder/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Threading.Tasks;
+
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DatabaseBuilder
+{
+    internal static class Program : Object
+    {
+        private static readonly String connectionString;
+
+        static Program()
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+
+            builder.AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        private static async Task Main(String[] args)
+        {
+            await CreateDatabase("MobileShop");
+
+
+        }
+
+        private static async Task CreateDatabase(String databaseName)
+        {
+            if (String.IsNullOrEmpty(databaseName))
+            {
+                throw new ArgumentNullException(nameof(databaseName));
+            }
+
+            var commandText = $"CREATE DATABASE [{databaseName}];";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                var command = new SqlCommand(commandText, connection);
+
+                await command.ExecuteNonQueryAsync();
+            }
+
+            Console.WriteLine($"Database {databaseName} successfully created.");
+        }
+
+        private static async Task DeleteDatabase(String databaseName)
+        {
+            if (String.IsNullOrEmpty(databaseName))
+            {
+                throw new ArgumentNullException(nameof(databaseName));
+            }
+
+            var commandText = $"DROP DATABASE [{databaseName}];";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                var command = new SqlCommand(commandText, connection);
+
+                await command.ExecuteNonQueryAsync();
+            }
+
+            Console.WriteLine($"Database {databaseName} successfully deleted.");
+        }
+
+        private static async Task CreateMobilesTable()
+        {
+            var commandText =
+            @"
+                CREATE TABLE []
+                (
+                    [Id] INT IDENTITY(1, 1),
+                    [Name] NVARCHAR(),
+                    CONSTRAINT 
+                );
+            ";
+        }
+    }
+}
